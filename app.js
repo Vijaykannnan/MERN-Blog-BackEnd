f684e9d255f5f2631c487d42e16d2a15be9cf4ab
Vijaykannnan <97211023+Vijaykannnan@users.noreply.github.com>
2023-05-06 20:56:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,16 +37,6 @@ app.use(cors({
     credentials: true,
 }));
 
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
